1e0651d83b86f96e10dfdd86b1fcf160a63bd696
Amanda Lemmons <amandalemmons@Amandas-MacBook-Pro.local>
2015-07-07 12:05:33 -0500

    filter categories for pet-friendly

--- config/routes.rb
+++ config/routes.rb
@@ -9,19 +9,6 @@ Rails.application.routes.draw do
   get '/logout' => 'session#destroy', as: :logout
 
 
-  # get '/dogparks' => 'yelps#dogparks', as: :dogparks
-  # get '/petstores' => 'yelps#petstores', as: :petservices
-  # get '/vets' => 'yelps#vets', as: :vets
-  # get '/groomers' => 'yelps#groomers', as: :groomers
-  # get '/pet-sitting' => 'yelps#pet_sitting', as: :pet_sitting
-  # get '/pet-training' => 'yelps#pet_training', as: :pet_training
-  # get '/hotels' => 'yelps#hotels', as: :hotels
-  # get '/restaurants' => 'yelps#restaurants', as: :restaurants
-  # get '/dog-walkers' => 'yelps#dogwalkers', as: :dogwalkers
-  # get '/animal-shelters' => 'yelps#animalshelters', as: :animalshelters
-
-
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
