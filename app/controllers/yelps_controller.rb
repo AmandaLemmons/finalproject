74b29ec24a2dbef32c394c2646b905182a6a6226
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-07-16 10:56:48 -0500

    saving and displaying users saved locations

--- app/controllers/yelps_controller.rb
+++ app/controllers/yelps_controller.rb
@@ -35,26 +35,69 @@ require 'yelp'
   end
 
 
-  def create
-
-  @saved_location = SavedLocation.new
-  @saved_location.business_id = params[:business_id]
-  @saved_location.user_id = @current_user.id
-  @saved_location.save
-
-end
+  # def create
+  # @current_user = User.find_by(id: session[:user_id])
+#   @saved_location = SavedLocation.new
+#   @saved_location.business_id = params[:business_id]
+#   @saved_location.user_id = @current_user.id
+#   @saved_location.save
+#   redirect_to login_path
+#
+# end
 
 
   def show
-    @saved_locations = SavedLocation.all
-     @businesses = @saved_locations.map do |sl|
-       Yelp.client.business(sl.business_id)
+    @saved_location = SavedLocation.new
+    @saved_location.business_id = params[:business_id]
+    @saved_location.user_id = @current_user.id
+    @saved_location.save
 
-  #   @businesses = @current_user.saved_locations.map do |sl|
+
+  # shows all of them.
+   @saved_locations = SavedLocation.all
+
+   @saved_locations_user_ids = @saved_locations.select do |saved_locations_user_id|
+     saved_locations_user_id.user_id == @current_user.id
+   end
+     @businesses = @saved_locations_user_ids.map do |sl|
+      Yelp.client.business(sl.business_id)
+
+   end
+
+
+  #  @saved_locations.user_id == @current_user.id
+
+  #  @businesses = @saved_locations.map do |sl|
+  #   Yelp.client.business(sl.business_id)
+  # #
+  #  end
+
+
+
+  #   @saved_locations = SavedLocation.all
+  #   # @saved_locations = SavedLocation.where(user_id: saved_location_id)
+  #
+  #  @current_user_loaction = @saved_locations.map do |user_location|
+  #     user_location.user_id == @current_user.id
+
+
+    # @businesses = @current_user_loaction.map do |sl|
+    #     Yelp.client.business(sl.business_id)
+    #  end
+
+
+    #
+    # @businesses = @saved_locations.map do |sl|
+    #    Yelp.client.business(sl.business_id)
+
+  # @current_user_loaction =  @saved_locations.map do |location|
+  #     location.user_id == @current_user.id
+  #   end
+  #
+  #    @businesses = @current_user_loaction.map do |sl|
   #      Yelp.client.business(sl.business_id)
-  #      redirect_to login_path
-    end
-  end
+
+   end
 
     # Need to make work. The first problem is with saved_locations. There isn't a saved location for the users
     # saved_locations = @saved_location.all
